5cdfafefe8d9b4a7b1b68987a7899e1ecdfe89aa
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-09-15 18:50:16 -0700

    activating routes with code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { AuthService } from './user/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pm-root',
@@ -21,10 +22,12 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   logOut(): void {
     this.authService.logout();
-    console.log('Log out');
+    // navigate will only replace the primary route segment
+    // I.E.: a primary with a secondary route /welcome(popup:messages) would retain the secondary (popup:messages)
+    this.router.navigate(['/welcome']);
   }
 }
