3aea4a9ca7828ca838a2c8960665be8a71a53b8d
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-09-15 21:53:18 -0700

    Defining route modules

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { LoginComponent } from './login.component';
-
+import { UserRoutingModule } from "./user-routing.module";
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule.forChild([{ path: 'login', component: LoginComponent }]),
+    UserRoutingModule
   ],
   declarations: [LoginComponent],
   exports:[LoginComponent]
