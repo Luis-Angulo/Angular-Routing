278979515b6a14c6a46ae81cac317907fdb92b55
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-09-17 20:27:33 -0700

    Get data for children

--- src/app/products/product-edit/product-edit-info.component.ts
+++ src/app/products/product-edit/product-edit-info.component.ts
@@ -5,16 +5,25 @@ import { NgForm } from '@angular/forms';
 import { Product } from '../product';
 
 @Component({
-  templateUrl: './product-edit-info.component.html'
+  templateUrl: './product-edit-info.component.html',
 })
 export class ProductEditInfoComponent implements OnInit {
-  @ViewChild(NgForm, {static: false}) productForm: NgForm;
+  // this grabs a reference from the template to the form to reset the form data on changes
+  @ViewChild(NgForm, { static: false }) productForm: NgForm;
 
   errorMessage: string;
-  product = { id: 1, productName: 'test', productCode: 'test', description: 'test' };
+  product: Product;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // data is shared across components using a reference to the same instance
+    // subscribe here rerenders the page when the data from the parent changes
+    this.route.parent.data.subscribe(
+      (resolvedData) => {
+        this.productForm?.reset();  // reset the form state on changes
+        this.product = resolvedData.product.product;
+      }
+    );
   }
 }
