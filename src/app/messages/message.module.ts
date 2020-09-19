2695e0f883636801a68a3bf1641356553f6b9904
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-09-19 01:37:57 -0700

    activating secondary routes

--- src/app/messages/message.module.ts
+++ src/app/messages/message.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -6,7 +7,13 @@ import { MessageComponent } from './message.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      // Secondary routes required a named outlet
+      // any secondary routes are defined in the url within parenthesis,
+      // they contain the name of the outlet and the name of the path to load
+      {path: 'messages', component: MessageComponent, outlet: 'popup'}
+    ])
   ],
   declarations: [
     MessageComponent
